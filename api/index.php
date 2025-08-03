6fab54eb162804ed9fd5093ebd552bd92c5b6c3f
Earbunsong <bunsong601@gmail.com>
2025-08-04 06:17:24 +0700

    add file to deploy

--- api/index.php
+++ api/index.php
@@ -1,25 +1,3 @@
 <?php
 
-// Set memory limit for Vercel
-ini_set('memory_limit', '1024M');
-ini_set('max_execution_time', '10');
-
-// Change to application root
-chdir(dirname(__DIR__));
-
-// Auto-loader
-require __DIR__ . '/../vendor/autoload.php';
-
-// Bootstrap Laravel application
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Handle the request
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . "/../public/index.php";
